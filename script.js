58d7f827feb02d42b0291c4fc27e8b1daf914235
Luciano Pires <lucianopires98@outlook.com>
2022-06-23 20:27:00 -0300

    Desafio 05

---
+++ script.js
@@ -0,0 +1,24 @@
+let nome = ["Luciano","Beatriz","Aline","Ana","Luan"];
+    console.log(nome);
+
+
+const novoNome = nome.unshift ("Jorge");
+    console.log(nome);
+
+const removeNome = nome.pop();   
+    console.log(nome);  
+
+
+const adicionarNome = nome.push("Eloah","Nice");
+    console.log(nome);
+
+
+const retirar = nome.shift();
+    console.log(nome);    
+
+
+
+let numero = [7,5,6,3,8,9,2,1,4] 
+
+const crescente = numero.sort();
+    console.log(numero)
